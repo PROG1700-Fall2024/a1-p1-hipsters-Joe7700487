d3d8c2b289fad6839d1c03e7ef2cf56524b87b79
joe <w0500154@nscc.ca>
2024-09-26 12:04:36 -0300

    finished

--- A1-P1-Hipsters.py
+++ A1-P1-Hipsters.py
@@ -3,14 +3,22 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    deliveryRate = 15
+    taxRate = 0.14
+    deliveryCost = float
+    totalCost = float
+    name = input("Enter name: ")
+    distance = float(input("Enter distance in km: "))
+    cost = float(input("Enter cost: "))
 
+    cost = cost + (cost * taxRate)
+    deliveryCost = deliveryRate * distance
+    totalCost = cost + deliveryCost
 
-
-
-
-
-
-
+    print("Purchase summary for", name)
+    print("Delivery cost: ${0:.2f}".format(deliveryCost))
+    print("Purchase cost: ${0:.2f}".format(cost))
+    print("Total cost: ${0:.2f}".format(totalCost))
     # YOUR CODE ENDS HERE
 
 main()
